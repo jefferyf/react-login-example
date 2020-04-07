3219244b5e7649561829272a372930d8ca9f9390
Jeff Miller <jeffery.f@gmail.com>
2020-04-07 14:01:55 -0500

    Updates

---
+++ precache-manifest.f42c362273a82874a158b68c0a78616b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d43422b43877df71837aa7c8bc5dd687",
+    "url": "/react-login-example/index.html"
+  },
+  {
+    "revision": "660e1d6a074889f4c57a",
+    "url": "/react-login-example/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "465dfb3c5d2aa99f04cf",
+    "url": "/react-login-example/static/js/2.52afe37b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-login-example/static/js/2.52afe37b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "660e1d6a074889f4c57a",
+    "url": "/react-login-example/static/js/main.fdcd912f.chunk.js"
+  },
+  {
+    "revision": "3363749e5140deacca1f",
+    "url": "/react-login-example/static/js/runtime-main.8c5b65c3.js"
+  }
+]);
